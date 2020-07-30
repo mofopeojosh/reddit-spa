3e779e67a1f45f51902d081e288b5a6e5f6398ca
Mofope Ojosh <mofopeojosh@gmail.com>
2020-07-30 18:16:56 +0100

    fix whitespace at the bottom of the screen

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app" class="app">
         <AppHeader/>
-        <main class="container my-5">
+        <main class="container py-5">
             <ListSubreddits />
         </main>
     </div>
