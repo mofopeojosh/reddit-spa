43ea72cdb6ffb592e3fdfe3d411e6d92d046481f
Mofope Ojosh <mofopeojosh@gmail.com>
2020-08-03 09:58:21 +0100

    remove min and max fields for date and votes - add range of votes - filter by single date only - make reset button bigger

--- src/components/SubredditItem.vue
+++ src/components/SubredditItem.vue
@@ -1,63 +1,70 @@
 <template>
-	<div class="row justify-content-center">
-		<div class="col-sm-7">
-			<div class="text-center">
-				<h4 class="my-0 text-capitalize">{{ subreddit.title }}</h4>
-				<small>{{ subreddit.slug }}</small>
+    <div class="row justify-content-center">
+        <div class="col-sm-7">
+            <div class="text-center">
+                <h4 class="my-0 text-capitalize">{{ subreddit.title }}</h4>
+                <small>{{ subreddit.slug }}</small>
+            </div>
+            <div class="my-4">
+                <input type="text" v-model="searchQuery"
+                       class="form-control form-control-lg rounded-pill w-100 border"/>
+            </div>
+        </div>
+        <div class="col-sm-12 col-sm-9">
+            <div class="d-flex flex-wrap align-items-end my-1">
+                <a href="#" @click="voteSort = 'up'">
+                    <span class="badge border mx-1" :class="voteSort == 'up' ? 'badge-success' : 'badge-secondary'">Highest
+                        Votes</span></a>
+                <a href="#" @click="voteSort = 'down'">
+                    <span class="badge border mx-1" :class="voteSort == 'down' ? 'badge-success' : 'badge-secondary'">Lowest
+                        Votes</span></a>
+                <a href="#" @click="dateSort = 'up'">
+                    <span class="badge border mx-1" :class="dateSort == 'up' ? 'badge-success' : 'badge-secondary'">Latest
+                    </span></a>
+                <a href="#" @click="dateSort = 'down'">
+                    <span class="badge border mx-1" :class="dateSort == 'down' ? 'badge-success' : 'badge-secondary'">Oldest
+                    </span></a>
+            </div>
+            <div class="form-inline mb-2">
+                <div class="d-flex flex-wrap justify-content-center">
+                    <div class="input-group input-group-sm m-1">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Date</span>
+                        </div>
+                        <input type="date" v-model="dateFilter" class="form-control">
+                    </div>
+                    <div class="input-group input-group-sm m-1">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">No of Upvotes</span>
+                        </div>
+                        <select v-model="voteRangeFilter" class="form-control">
+                            <option value="">-- Select a range --</option>
+                            <option v-for="(option, index) in Object.keys(voteFilterOptions)"
+                                    :value="option"
+                                    :key="index">
+                                {{ option }}
+                            </option>
+                        </select>
+                    </div>
+                </div>
+				<button class="btn btn-sm border btn-light mx-1" @click="clearFilter">Clear filters</button>
+
 			</div>
-			<div class="my-4">
-				<input type="text" v-model="searchQuery" class="form-control form-control-lg rounded-pill w-100 border"/>
-			</div>
-		</div>
-		<div class="col-sm-12 col-sm-9">
-			<div class="d-flex flex-wrap align-items-end my-1">
-				<a href="#" @click="voteSort = 'up'">
-					<span class="badge border mx-1" :class="voteSort == 'up' ? 'badge-success' : 'badge-secondary'">Highest
-						Votes</span></a>
-				<a href="#" @click="voteSort = 'down'">
-					<span class="badge border mx-1" :class="voteSort == 'down' ? 'badge-success' : 'badge-secondary'">Lowest
-						Votes</span></a>
-				<a href="#" @click="dateSort = 'up'">
-					<span class="badge border mx-1" :class="dateSort == 'up' ? 'badge-success' : 'badge-secondary'">Latest
-					</span></a>
-				<a href="#" @click="dateSort = 'down'">
-					<span class="badge border mx-1" :class="dateSort == 'down' ? 'badge-success' : 'badge-secondary'">Oldest
-					</span></a>
-				<a href="#" @click="clearFilter"><span class="badge border badge-light mx-1">Clear</span></a>
-			</div>
-			<div class="form-inline mb-2">
-				<div class="d-flex flex-wrap justify-content-center">
-					<div class="input-group input-group-sm m-1">
-						<div class="input-group-prepend">
-							<span class="input-group-text">Date</span>
-						</div>
-						<input type="date" v-model="minDateFilter" class="form-control">
-						<input type="date" v-model="maxDateFilter" class="form-control">
-					</div>
-					<div class="input-group input-group-sm m-1">
-						<div class="input-group-prepend">
-							<span class="input-group-text">No of Upvotes</span>
-						</div>
-						<input type="number" v-model="minVoteFilter" class="form-control">
-						<input type="number" v-model="maxVoteFilter" class="form-control">
-					</div>
-				</div>
-			</div>
-			<template v-if="subredditPosts.length">
-				<div class="mb-4" v-for="(subreddit, index) in subredditPosts" :key="index">
-					<PostItem :post="subreddit"/>
-				</div>
-			</template>
-			<div class="my-5 py-5 text-center" v-else-if="allPosts.length == 0">
-				<div class="spinner-border" role="status">
-					<span class="sr-only">Loading...</span>
-				</div>
-			</div>
-			<div class="my-5 py-5 text-center" v-else>
-				<p>No posts found</p>
-			</div>
-		</div>
-	</div>
+            <template v-if="subredditPosts.length">
+                <div class="mb-4" v-for="(subreddit, index) in subredditPosts" :key="index">
+                    <PostItem :post="subreddit"/>
+                </div>
+            </template>
+            <div class="my-5 py-5 text-center" v-else-if="allPosts.length == 0">
+                <div class="spinner-border" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div>
+            <div class="my-5 py-5 text-center" v-else>
+                <p>No posts found</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -72,8 +79,8 @@ export default {
         },
         posts: {
             type: Array,
-			default: () => ([])
-		}
+            default: () => ([])
+        }
     },
     components: {PostItem},
     data() {
@@ -82,10 +89,16 @@ export default {
             noOfVotesFilter: '',
             dateSort: '',
             voteSort: null,
-            minVoteFilter: '',
-            maxVoteFilter: '',
-            minDateFilter: '',
-            maxDateFilter: '',
+            dateFilter: '',
+            voteRangeFilter: '',
+            voteFilterOptions: {
+                '0 - 100': {min: 0, max: 100},
+                '101 - 500': {min: 100, max: 500},
+                '501 - 1000': {min: 500, max: 1000},
+                '1001 - 10000': {min: 1000, max: 10000},
+                '10001 - 50000': {min: 10000, max: 50000},
+                'More than 50000': {min: 50000, max: null},
+            }
         };
     },
     computed: {
@@ -99,10 +112,10 @@ export default {
             if (this.dateSort) {
                 return this.sortPostsByDate();
             }
-            if (this.minVoteFilter || this.maxVoteFilter) {
+            if (this.voteRangeFilter) {
                 return this.filterPostsByVotes();
             }
-            if (this.minDateFilter || this.maxDateFilter) {
+            if (this.dateFilter) {
                 return this.filterPostsByDate();
             }
             return this.allPosts;
@@ -137,30 +150,33 @@ export default {
             return posts;
         },
         filterPostsByVotes() {
+            const selectedFilter = this.voteFilterOptions[this.voteRangeFilter];
+            const minVotes = selectedFilter.min;
+            const maxVotes = selectedFilter.max;
             const posts = this.allPosts.filter(post => {
-                return this.minVoteFilter <= post.data.ups && this.maxVoteFilter >= post.data.ups;
+                if (maxVotes) {
+                    return minVotes <= post.data.ups && maxVotes >= post.data.ups;
+                }
+                return minVotes <= post.data.ups;
+
             });
             return posts;
-		},
-		filterPostsByDate() {
+        },
+        filterPostsByDate() {
             const posts = this.allPosts.filter(post => {
-                const today = new Date();
-                const minDate = this.minDateFilter ? new Date(this.minDateFilter).setHours(0,0,0,0) : today;
-                const maxDate = this.maxDateFilter ? new Date(this.maxDateFilter).setHours(0,0,0,0) : today;
-                const createdDate =new Date(post.data.created_utc * 1000).setHours(0,0,0,0);
-                return minDate <= createdDate && maxDate >= createdDate;
+                const date = new Date(this.dateFilter).setHours(0, 0, 0, 0);
+                const createdDate = new Date(post.data.created_utc * 1000).setHours(0, 0, 0, 0);
+                return date == createdDate;
             });
             return posts;
-		},
-		clearFilter() {
+        },
+        clearFilter() {
             this.searchQuery = '';
             this.voteSort = null;
             this.dateSort = null;
-            this.minDateFilter = '';
-            this.maxDateFilter = '';
-            this.minVoteFilter = '';
-            this.maxDateFilter = '';
-		}
+            this.dateFilter = '';
+            this.voteRangeFilter = '';
+        }
     }
 };
 </script>
